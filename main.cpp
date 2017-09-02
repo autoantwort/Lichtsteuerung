d94023505481f2fa0bde5d9878bd0cd6bc06c745
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2017-09-02 18:25:59 +0200

    Implement JSON loading and saving

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <id.h>
-
+#include <device.h>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     ID id;
+
 
     qDebug() << "Hello ich bin cooler \n"<<id.value();
     return a.exec();
