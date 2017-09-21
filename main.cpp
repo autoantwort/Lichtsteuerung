5a95d1f095d29d321bef54e11621d39414b06807
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2017-09-21 14:07:19 +0200

    Extract the Name and Description property in a NamedObject class. Adding Support for QObject

--- main.cpp
+++ main.cpp
@@ -2,12 +2,35 @@
 #include <QDebug>
 #include <id.h>
 #include <device.h>
+#include "channel.h"
+#include "applicationdata.h"
+#include "programm.h"
 int main(int argc, char *argv[])
 {
+
+    const auto filename = "QTJSONFile.json";
+    QFile file(filename);
+
+    ApplicationData::loadData(file);
+    //ProgrammPrototype p(*IDBase<DevicePrototype>::getAllIDBases().begin(), "Test","Mein erstes Programm");
+    Programm p("Mein erstes Programm");
+    p.addDeviceProgramm(*IDBase<Device>::getAllIDBases().begin(),*IDBase<ProgrammPrototype>::getAllIDBases().begin(),0.5);
+    //DevicePrototype c("Scanner");
+    //c.addChannel(0,"Speed");
+    //c.addChannel(1,"RED");
+    //c.addChannel(2,"GREEN");
+    //c.addChannel(3,"BLUE");
+    new Channel(1);
+    new Channel(3);
+    delete new Channel(2);
     QCoreApplication a(argc, argv);
     ID id;
 
+    ApplicationData::saveData(file);
 
+    //IDBase<Device>::deleter.hello();
+    //IDBase<Channel>::deleter.hello();
     qDebug() << "Hello ich bin cooler \n"<<id.value()<<' '<<IDBase<Device>::getIDBaseObjectByID(0)<<'\n';
-    return a.exec();
+    //return a.exec();
+    return 0;
 }
