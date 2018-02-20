0fddc9844ab99e09068e40d90c55dacb8fab4b14
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2018-02-20 22:52:26 +0100

    The Driver Dummy

---
+++ test/DriverDummy.h
@@ -0,0 +1,32 @@
+#ifndef DRIVER_DUMMY_H
+#define DRIVER_DUMMY_H
+
+#include "HardwareInterface.h"
+#include <QDebug>
+#include <iostream>
+
+
+class DriverDummy : public AbstractHardwareInterface
+{
+    unsigned char data[512];
+public:
+    DriverDummy () {}
+
+    virtual bool init(){qDebug()<<"initDriver";}
+    virtual bool isFunctioning(){return true;}
+
+protected:
+
+    virtual void setValuesDeviceDriver() {
+        qDebug()<<"set values";
+        setValues(data,512,time);
+        auto out = qDebug();
+        for (int var = 0; var < 20; ++var) {
+            out << (int)*(data+var);
+        }
+    }
+};
+
+
+
+#endif /*DRIVER_DUMMY_H*/
