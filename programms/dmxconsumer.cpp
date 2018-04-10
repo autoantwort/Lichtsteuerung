2036decf19311d16d8efd741edd2c8b5ace84966
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2018-04-10 15:58:26 +0200

    Move to namespace

--- programms/dmxconsumer.cpp
+++ programms/dmxconsumer.cpp
@@ -1,22 +1,26 @@
 #include "dmxconsumer.h"
 
-DMXConsumer::DMXConsumer()
-{
+namespace Modules {
 
-}
+    DMXConsumer::DMXConsumer()
+    {
 
-void DMXConsumer::setInputSize(unsigned int size){
-    channel.resize(size,-1);
-}
+    }
 
-void DMXConsumer::start(){}
+    void DMXConsumer::setInputSize(unsigned int size){
+        channel.resize(size,-1);
+    }
 
-void DMXConsumer::show(void * data){
-    brightness_t * b = static_cast<brightness_t*>(data);
-    for(auto i = channel.cbegin();i!=channel.cend();++i,++b){
-        if(*i>0){
-            //dmxData[*i] = *b;
-            //TODO;
+    void DMXConsumer::start(){}
+
+    void DMXConsumer::show(void * data){
+        brightness_t * b = static_cast<brightness_t*>(data);
+        for(auto i = channel.cbegin();i!=channel.cend();++i,++b){
+            if(*i>0){
+                //dmxData[*i] = *b;
+                //TODO;
+            }
         }
     }
+
 }
--- programms/dmxconsumer.h
+++ programms/dmxconsumer.h
@@ -4,16 +4,19 @@
 #include "consumer.h"
 #include <vector>
 
-class DMXConsumer : public Modules::Consumer
-{
-    std::vector<short> channel;
-public:
-    DMXConsumer();    
-    virtual ValueType getInputValueType()override{return Brightness;}
-    virtual void setInputSize(unsigned int) override;
-    virtual void start()override;
-    virtual void show(void * data) override;
-    const std::vector<short> & getChannelAssoziation()const{return channel;}
-};
+namespace Modules {
 
+    class DMXConsumer : public Modules::Consumer
+    {
+        std::vector<short> channel;
+    public:
+        DMXConsumer();
+        virtual ValueType getInputValueType()override{return Brightness;}
+        virtual void setInputSize(unsigned int) override;
+        virtual void start()override;
+        virtual void show(void * data) override;
+        const std::vector<short> & getChannelAssoziatfion()const{return channel;}
+    };
+
+}
 #endif // DMXCONSUMER_H
