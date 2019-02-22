8fdc186889729ad4c532ae50f3e56bb3c617869f
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2019-02-22 16:26:43 +0100

    Use Windows API instead of QT to load the library, because QLibrary will not search in the folder of the lib for dependencies

--- src/driver.cpp
+++ src/driver.cpp
@@ -6,6 +6,9 @@
 #include "dmxchannelfilter.h"
 #include <QDir>
 #include <cstring>
+#ifdef Q_OS_WIN
+#include <windows.h>
+#endif
 
 HardwareInterface * driver = nullptr;
 
@@ -27,7 +30,29 @@ namespace Driver {
     bool loadDriver(QString path){
 #if defined(Q_OS_WIN)
     typedef HardwareInterface * (*getDriverFunc)();
-    getDriverFunc getDriver =  reinterpret_cast<getDriverFunc>(QLibrary::resolve(path,"getDriver"));
+    // we have to load the library with the Windows API to handle dependecies in the same folder, see https://bugreports.qt.io/projects/QTBUG/issues/QTBUG-74001
+    path.replace("/","\\");
+    auto pathC = path.toStdString();
+    auto handle = LoadLibraryExA(pathC.c_str(),nullptr, LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR|LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+    if(handle == nullptr){
+        auto errorCode = GetLastError();
+        qDebug() << "Loading of lib failed, error code: "<<errorCode;
+        if(errorCode == 126)
+            ErrorNotifier::get()->newError("Fehler beim laden der DLL, Abhängigkeiten zu anderen DLLs konnten nicht geladen werden.");
+        else{
+            ErrorNotifier::get()->newError("Fehler beim laden der DLL. Error code: " + QString::number(errorCode));
+        }
+        return false;
+    }
+    auto func = GetProcAddress(handle,"getDriver");
+    if(func == nullptr){
+        auto errorCode = GetLastError();
+        qDebug() << "Loading of lib failed, error code: "<<errorCode;
+        ErrorNotifier::get()->newError("Fehler beim laden der Funktion \"getDriver\" aus der DLL, Fehlercode: " + QString::number(errorCode));
+        return false;
+    }
+    getDriverFunc getDriver = reinterpret_cast<getDriverFunc>(func);
+    //getDriverFunc getDriver =  reinterpret_cast<getDriverFunc>(QLibrary::resolve(path,"getDriver"));
     if(getDriver!=nullptr){
         HardwareInterface * inter = getDriver();
         if(inter != nullptr){
