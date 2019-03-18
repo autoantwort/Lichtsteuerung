34e6e5775033bf81d2b3c2b767d762810037daab
Leander Schulten <Leander.Schulten@hilton.rwth-aachen.de>
2019-03-18 01:28:50 +0100

    Add possibility to log driver data. Important: Fix wrong index of dmx data, e.g. old channel 79 is now 80

--- src/driver.cpp
+++ src/driver.cpp
@@ -7,11 +7,25 @@
 #include <QDir>
 #include <cstring>
 #include "programms/dmxconsumer.h"
+
+//#define LOG_DRIVER
+
+#ifdef LOG_DRIVER
+#include <fstream>
+#include <QDateTime>
+#include <algorithm>
+#include "device.h"
+#endif
 #ifdef Q_OS_WIN
 #include <windows.h>
 #endif
 
 HardwareInterface * driver = nullptr;
+
+#ifdef LOG_DRIVER
+std::ofstream debugOutput;
+std::vector<unsigned char> oldData;
+#endif
 
 namespace Driver {
 
@@ -61,12 +75,46 @@ namespace Driver {
                 qDebug()<<s;
                 ErrorNotifier::get()->newError(s);
             });
+#ifdef LOG_DRIVER
+            debugOutput.open("driverOutput" + QDateTime::currentDateTime().toString("dd.MM.yyyy HH.mm.ss").toStdString() + ".csv");
+            if(!debugOutput.is_open()){
+                qDebug() << "Can not open " << "C:\\Program Files\\Lichtsteuerung\\data\\driverOutput" << QDateTime::currentDateTime().toString("dd.MM.yyyy HH.mm.ss") << ".csv";
+            }else{
+                std::vector<std::string> names(512,"Not set");
+                for(int i = 0 ; i< names.size();++i){
+                    names[i] += std::to_string(i+1);
+                }
+                for(const auto & i : IDBase<Device>::getAllIDBases()){
+                    if(i->getStartDMXChannel() > 1)
+                     names[i->getStartDMXChannel()-1] = i->getName().toStdString();
+                }
+                debugOutput << ';';
+                std::for_each(std::begin(names),std::end(names),[&](const auto & v){
+                    debugOutput <<v << ';';
+                });
+                debugOutput << '\n';
+            }
+#endif
             inter->setSetValuesCallback([](unsigned char* values, int size, double time){
+#ifdef LOG_DRIVER
+                oldData.resize(size);
+#endif
                 std::memset(values,0,size);
                 DMXChannelFilter::initValues(values,size);
                 Programm::fill(values,size,time);
                 Modules::DMXConsumer::fillWithDMXConsumer(values,size);
                 DMXChannelFilter::filterValues(values,size);
+                std::copy(values+1, values+size,values);
+#ifdef LOG_DRIVER
+                if(debugOutput.is_open() && !std::equal(std::begin(oldData),std::end(oldData),values)){
+                    debugOutput << std::to_string(time) << ';';
+                    std::for_each(values,values+size,[&](const auto & v){
+                        debugOutput << static_cast<int>(v) << ';';
+                    });
+                    debugOutput << '\n';
+                    std::copy(values,values+size,std::begin(oldData));
+                }
+#endif
             });
 
             if(driver){
