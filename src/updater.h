39f28967a5dc1eb96da8c8fa41191fb70250bfed
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2019-08-09 00:16:07 +0200

    Fix wrong version in strings from 5.12.3 to 5.13.0

--- src/updater.h
+++ src/updater.h
@@ -10,7 +10,7 @@ class Updater : public QObject{
     Q_OBJECT
     Q_PROPERTY(int progress READ getUpdateProgress NOTIFY updateProgressChanged)
     const inline static QString VERSION_FILE_NAME = QStringLiteral("version.txt");
-    const inline static QString NAME_OF_DEPLOY_FOLDER = QStringLiteral("windows-release-5.12.3");
+    const inline static QString NAME_OF_DEPLOY_FOLDER = QStringLiteral("windows-release-5.13.0");
     const inline static QString WINDOWS_INSTALLER_NAME = QStringLiteral("WindowsInstaller.exe");
     QString versionDownloadURL = QStringLiteral("https://git.rwth-aachen.de/leander.schulten/Lichtsteuerung/-/jobs/artifacts/windows-release/download?job=version");
     QString deployDownloadURL = QStringLiteral("https://git.rwth-aachen.de/leander.schulten/Lichtsteuerung/-/jobs/artifacts/windows-release/download?job=deploy");
