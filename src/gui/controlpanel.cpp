ff4890f3c99a5806455db56a9c67ddf76af90dcc
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2019-08-09 04:06:03 +0200

    ControlPane: Applied CodeModel proposals

--- src/gui/controlpanel.cpp
+++ src/gui/controlpanel.cpp
@@ -1,17 +1,17 @@
 #include "controlpanel.h"
-#include "controlitemdata.h"
 #include "controlitem.h"
-#include <QJsonArray>
-#include <exception>
-#include <QQmlContext>
+#include "controlitemdata.h"
 #include "errornotifier.h"
+#include <QJsonArray>
+#include <QQmlContext>
+#include <exception>
 
 namespace GUI{
 
 ControlPanel * ControlPanel::lastCreated = nullptr;
 QQmlEngine * ControlPanel::engine = nullptr;
 
-ControlPanel::ControlPanel():programm(engine,QUrl("qrc:/qml/ControlPane/ProgrammControl.qml")),switchGroup(engine,QUrl("qrc:/qml/ControlPane/SwitchGroupControl.qml")),dimmerGroup(engine,QUrl("qrc:/qml/ControlPane/DimmerGroupControl.qml")),programBlock(engine,QUrl("qrc:/qml/ControlPane/ProgramBlockControl.qml"))
+ControlPanel::ControlPanel():programm(engine,QUrl(QStringLiteral("qrc:/qml/ControlPane/ProgrammControl.qml"))),switchGroup(engine,QUrl(QStringLiteral("qrc:/qml/ControlPane/SwitchGroupControl.qml"))),dimmerGroup(engine,QUrl(QStringLiteral("qrc:/qml/ControlPane/DimmerGroupControl.qml"))),programBlock(engine,QUrl(QStringLiteral("qrc:/qml/ControlPane/ProgramBlockControl.qml")))
 {
     lastCreated = this;
     if(engine==nullptr){
@@ -34,51 +34,53 @@ ControlPanel::ControlPanel():programm(engine,QUrl("qrc:/qml/ControlPane/Programm
 
 void ControlPanel::writeJsonObject(QJsonObject &o){
     QJsonArray a;
-    for(auto i = childItems().cbegin();i!=childItems().cend();++i){
-        const auto data = (**i).property("controlData");
+    auto children = childItems();
+    for(auto i : children){
+        const auto data = (*i).property("controlData");
         if(data.value<ControlItemData*>() != nullptr){
-            static_cast<ControlItem*>(*i)->savePositionInControlData();
+            dynamic_cast<ControlItem*>(i)->savePositionInControlData();
             QJsonObject o;
             data.value<ControlItemData*>()->writeJsonObject(o);
             a.append(o);
         }
     }
-    o.insert("ControlItems",a);
+    o.insert(QStringLiteral("ControlItems"),a);
 }
 
 void ControlPanel::createControlItem(QQmlComponent &component, ControlItemData *data){
-    QQmlContext* context = new QQmlContext(engine->rootContext(),engine);
-    context->setContextProperty("__controlData",data);
-    ControlItem * item = static_cast<ControlItem*>(component.create(context));
+    QQmlContext context(engine->rootContext(),engine);
+    context.setContextProperty(QStringLiteral("__controlData"),data);
+    auto * item = qobject_cast<ControlItem*>(component.create(&context));
     item->setParentItem(this);
     data->setParent(item);
     QQmlEngine::setObjectOwnership(item,QQmlEngine::JavaScriptOwnership);
 }
 
 void ControlPanel::loadFromJsonObject(const QJsonObject &o){
-    const auto array = o["ControlItems"].toArray();
-    for(const auto o_ : array){
+    const auto array = o[QStringLiteral("ControlItems")].toArray();
+    for(const auto& o_ : array){
         const auto o = o_.toObject();
         try {
-            if(o["type"].toInt()==ControlItemData::PROGRAMM){
+            if(o[QStringLiteral("type")].toInt()==ControlItemData::PROGRAMM){
                 createControlItem(programm,new ProgrammControlItemData(o));
-            }else if(o["type"].toInt()==ControlItemData::SWITCH_GROUP){
+            }else if(o[QStringLiteral("type")].toInt()==ControlItemData::SWITCH_GROUP){
                 createControlItem(switchGroup,new SwitchGroupControlItemData(o));
-            }else if(o["type"].toInt()==ControlItemData::DIMMER_GROUP){
+            }else if(o[QStringLiteral("type")].toInt()==ControlItemData::DIMMER_GROUP){
                 createControlItem(dimmerGroup,new DimmerGroupControlItemData(o));
-            }else if(o["type"].toInt()==ControlItemData::PROGRAM_BLOCK){
+            }else if(o[QStringLiteral("type")].toInt()==ControlItemData::PROGRAM_BLOCK){
                 createControlItem(programBlock,new ProgramBlockControlItemData(o));
             }
         } catch (const std::runtime_error &e) {
             const auto typeNames = {"Program","SwitchGroup","DimmerGroup","ProgramBlock"};
-            ErrorNotifier::showError(QString("Error while creating a ControlItem of Type ") + typeNames.begin()[o["type"].toInt()] + "\nCause: " + e.what());
+            ErrorNotifier::showError(QStringLiteral("Error while creating a ControlItem of Type ") + typeNames.begin()[o[QStringLiteral("type")].toInt()] + "\nCause: " + e.what());
         }
     }
 }
 
 void ControlPanel::addProgrammControl(DMX::Programm *p){
-    if(p==nullptr)
+    if(p==nullptr) {
         throw std::runtime_error("Nullpointer für neues Object");
+    }
     createControlItem(programm,new ProgrammControlItemData(p));
 }
 
@@ -90,8 +92,9 @@ void ControlPanel::addDimmerGroupControl(){
 }
 
 void ControlPanel::addProgramBlockControl(Modules::ProgramBlock *p){
-    if(p==nullptr)
+    if(p==nullptr) {
         throw std::runtime_error("Nullpointer für neues Object");
+    }
     createControlItem(programBlock,new ProgramBlockControlItemData(p));
 }
 
