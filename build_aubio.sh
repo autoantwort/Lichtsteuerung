13b367988ba8c9156ca1fa52f1e18efa985e8361
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2019-08-29 04:14:48 +0200

    Fix Aubio CI build.

--- build_aubio.sh
+++ build_aubio.sh
@@ -10,6 +10,11 @@ cd aubio.git
 # get waf to build
 ./scripts/get_waf.sh
 # build
-./waf configure --disable-wavwrite --disable-wavread --msvc_version='mingw' --prefix ""
-./waf build 
-./waf install --destdir "$PWD/../src/lib/aubio"
+if [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+    # we are on windows
+    ./waf configure --disable-wavwrite --disable-wavread --msvc_version='mingw' --prefix= ""
+else
+    # we are on linux and cross compile
+    ./waf configure --disable-wavwrite --disable-wavread  --with-target-platform=win64 --prefix= ""
+fi
+./waf install --disable-tests --disable-examples --destdir "$PWD/../src/lib/aubio"
