52f67c8be4c02c85d8d485cac279f3d47ac982a3
Leander Schulten <leander.schulten@rwth-aachen.de>
2019-09-19 05:38:14 +0200

    Fix CI build

--- src/lib/boost/installOrBuild.sh
+++ src/lib/boost/installOrBuild.sh
@@ -26,6 +26,13 @@ elif [[ "$OSTYPE" == "msys" ]] || ! [[ -z "$GITLAB_CI" ]]; then
     # windows or gitlab ci
     echo "You are on Windows"
     GIT_DIR="boost.git"
+    if ! [[ -z "$GITLAB_CI" ]]; then
+        # use the cross compiler on the ci
+        TOOLSET="gcc-mxe"
+    else
+        TOOLSET="gcc"
+    fi
+
     if ! [ -d "$GIT_DIR" ]; then
         echo "We have to clone the repo, this will take approximately 6.5 minutes"
         git clone --depth 1 --shallow-submodules --recurse-submodules --jobs 32 https://github.com/boostorg/boost.git "$GIT_DIR"
@@ -36,13 +43,14 @@ elif [[ "$OSTYPE" == "msys" ]] || ! [[ -z "$GITLAB_CI" ]]; then
             OLD_CXX="$CXX"
             export CXX="/usr/bin/g++"
         fi
+        # configure the build
         ./bootstrap.sh
-        ls
-         #echo "using gcc : mxe : ${TARGET}-g++ : <rc>${TARGET}-windres <archiver>${TARGET}-ar <ranlib>${TARGET}-ranlib ;" > 'user-config.jam'
         if ! [[ -z "$GITLAB_CI" ]]; then
             # set to old value, so that the cross compiling works
             export CXX="$OLD_CXX"
-            echo "CXX: $CXX"
+            # copied from https://github.com/mxe/mxe/blob/90da28981e48b9c8e51d9ad20c715f1869a9e67b/src/boost.mk#L30
+            TARGET="x86_64-w64-mingw32.shared.posix"
+            echo "using gcc : mxe : ${TARGET}-g++ : <rc>${TARGET}-windres <archiver>${TARGET}-ar <ranlib>${TARGET}-ranlib ;" > 'project-config.jam'
         fi
     else
         cd "$GIT_DIR"
@@ -52,7 +60,7 @@ elif [[ "$OSTYPE" == "msys" ]] || ! [[ -z "$GITLAB_CI" ]]; then
         EXE=".exe" # windows need the .exe postfix
     fi
     echo "Build boost, this will take approximately 3 minutes, if boost is not already build."
-    ./b2$EXE --prefix=../ --layout=tagged --with-coroutine --with-context target-os=window toolset=gcc link=shared threading=multi runtime-link=shared address-model=64 install
+    ./b2$EXE --prefix=../ -q --layout=tagged --with-coroutine --with-context abi=ms architecture=x86 binary-format=pe target-os=windows toolset=$TOOLSET link=shared threading=multi runtime-link=shared address-model=64 install
     echo "Boost build is finished"
 else
     # linux?
