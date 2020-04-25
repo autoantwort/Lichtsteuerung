c89d21e72806e9326563767a01a5d3d28226e993
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2020-04-25 15:20:12 +0200

    Deploy: The release folder should not contain the Qt Version. I don't what to change that every time the Qt Version changes.

--- src/updater.h
+++ src/updater.h
@@ -11,7 +11,7 @@ class Updater : public QObject{
     Q_PROPERTY(int progress READ getUpdateProgress NOTIFY updateProgressChanged)
     Q_PROPERTY(UpdaterState state READ getState NOTIFY stateChanged)
     const inline static QString VERSION_FILE_NAME = QStringLiteral("version.txt");
-    const inline static QString NAME_OF_DEPLOY_FOLDER = QStringLiteral("windows-release-5.13.0");
+    const inline static QString NAME_OF_DEPLOY_FOLDER = QStringLiteral("windows-release");
     const inline static QString WINDOWS_INSTALLER_NAME = QStringLiteral("WindowsInstaller.exe");
     QString versionDownloadURL = QStringLiteral("https://git.rwth-aachen.de/leander.schulten/Lichtsteuerung/-/jobs/artifacts/windows-release/download?job=version");
     QString deployDownloadURL = QStringLiteral("https://git.rwth-aachen.de/leander.schulten/Lichtsteuerung/-/jobs/artifacts/windows-release/download?job=deploy");
