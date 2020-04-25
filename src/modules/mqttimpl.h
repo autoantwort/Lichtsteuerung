8633228b78e3ae8620d6e1c0992bb090607c4aa7
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2020-04-25 02:15:42 +0200

    Modules: Add MQTT support
    Merge remote-tracking branch 'origin/wip_need_sanitizer' into controllerInQThread

---
+++ src/modules/mqttimpl.h
@@ -0,0 +1,37 @@
+#ifndef MQTTIMPL_H
+#define MQTTIMPL_H
+
+#include "mqtt.hpp"
+#include <QtMqtt/QMqttClient>
+#include <atomic>
+#include <memory>
+#include <optional>
+#include <set>
+#include <tuple>
+
+namespace Modules {
+
+class MqttImpl : public detail::IMqttClientImpl {
+    std::unique_ptr<QMqttClient> client;
+    std::optional<std::tuple<QString, QByteArray, bool>> lastWillDelayed;
+    std::optional<std::tuple<QString, int>> connectDelayed;
+    std::set<std::pair<std::string, const char *>> callbacks;
+    std::vector<std::pair<std::string, std::function<void(std::string)>>> queried;
+
+public:
+    MqttImpl();
+    ~MqttImpl() override;
+    void setLastWillMessage(const std::string &topic, const std::string &message, bool retain) override;
+    void connect(const std::string &host, int port) override;
+    void publishMessage(const std::string &topic, const std::string &message) override;
+    void publishValue(const std::string &topic, const std::string &message) override;
+    void subscribe(const std::string &topic, std::function<void(std::string)> callback) override;
+    MqttClientStatus status() override;
+
+protected:
+    void publish(const std::string &topic, const std::string &message, bool retain);
+};
+
+} // namespace Modules
+
+#endif // MQTTIMPL_H
