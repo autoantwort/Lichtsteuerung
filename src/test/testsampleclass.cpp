fcb7a96d1e0ae720485ce746ecfb105239a99f2b
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-08-30 01:49:19 +0200

    iterators are not pointers

--- src/test/testsampleclass.cpp
+++ src/test/testsampleclass.cpp
@@ -10,23 +10,23 @@ void testSampleClass() {
     std::array<int, 10> v;
     std::iota(v.begin(), v.end(), 0);
     // for_each(v.cbegin(),v.cend(),[](auto i ){qDebug() << i;});
-    s.addData(v.begin(), v.end());
+    s.addData(v.data(), v.data() + v.size());
     // for_each(s.getArray().cbegin(),s.getArray().cend(),[](auto i ){qDebug() << i;});
     for (int i = 0; i < 10; ++i) {
         Q_ASSERT(s.getArray().at(i) == i);
     }
-    s.addData(v.begin(), v.end(), 1, 1);
+    s.addData(v.data(), v.data() + v.size(), 1, 1);
     // for_each(s.getArray().cbegin(),s.getArray().cend(),[](auto i ){qDebug() << i;});
     for (int i = 5, j = 1; i < 10; ++i, j += 2) {
         // qDebug()<<i << ':'<<j;
         Q_ASSERT(s.getArray().at(i) == j);
     }
-    s.addData(v.begin(), v.end(), 4, 4);
+    s.addData(v.data(), v.data() + v.size(), 4, 4);
     // qDebug();
     // for_each(s.getArray().cbegin(),s.getArray().cend(),[](auto i ){qDebug() << i;});
     Q_ASSERT(s.data()[8] == 4);
     Q_ASSERT(s.data()[9] == 9);
-    s.addData(v.begin(), v.end(), 8, 2);
+    s.addData(v.data(), v.data() + v.size(), 8, 2);
     // qDebug();
     // for_each(s.getArray().cbegin(),s.getArray().cend(),[](auto i ){qDebug() << i;});
     Q_ASSERT(s.data()[8] == 9);
