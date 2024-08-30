a4ad6c38d543f368ae94ab961cdc2abe8db1ebe1
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2024-08-30 02:10:17 +0200

    Try newer cmake version in segvcatch to fix windows ci

--- overlay-ports/segvcatch/portfile.cmake
+++ overlay-ports/segvcatch/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF afe79c49f7d996e2b3143199e6cef69f406247bb
     SHA512 2229671f62bf5a9f7177c3ecbda2f5172adc4d8a529b66cc4f5a7d1eb426c0b08d94fad58576d7a86d48b0ca5826ed5dd30f1838fdbef4416902ca799a6423aa
     HEAD_REF master
+    PATCHES
+        newer-cmake.patch
 )
 
 vcpkg_cmake_configure(
