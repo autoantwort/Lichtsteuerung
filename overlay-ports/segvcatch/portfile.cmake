b4ed5075e9b479a1122f67fd1f963563a0aea1f1
autoantwort <Leander.schulten@rwth-aachen.de>
2024-12-27 12:22:00 +0100

    test

--- overlay-ports/segvcatch/portfile.cmake
+++ overlay-ports/segvcatch/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 2229671f62bf5a9f7177c3ecbda2f5172adc4d8a529b66cc4f5a7d1eb426c0b08d94fad58576d7a86d48b0ca5826ed5dd30f1838fdbef4416902ca799a6423aa
     HEAD_REF master
     PATCHES
-        newer-cmake.patch
+        #newer-cmake.patch
 )
 
 vcpkg_cmake_configure(
